40ceaf7e84b48b8b6b71d8dc8f4849826fbacf6b
William Manahan-Makley <info@willmakley.dev>
2023-04-09 13:06:45 -0400

    git rid of hardcoded prod assets, as these are now compiled on the fly

--- static/_app/immutable/chunks/1.d4102631.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.ed1e6a13.js";export{t as component};
--- static/_app/immutable/chunks/2.22fa4e9f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.d46fcfe2.js";export{t as component};
